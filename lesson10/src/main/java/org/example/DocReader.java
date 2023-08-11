00a5fd29dd02615cd122b20b4197e68b5254e8af
Oleg <01technique@gmail.com>
2023-08-11 20:38:18 +0300

    Commit

--- lesson10/src/main/java/org/example/DocReader.java
+++ lesson10/src/main/java/org/example/DocReader.java
@@ -5,30 +5,30 @@ import java.util.Scanner;
 
 public class DocReader {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите путь к файлу:");
-            String path = scanner.nextLine();
-            LineNumberReader reader = new LineNumberReader(new BufferedReader(new FileReader(path)));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу:");
+        String path = scanner.nextLine();
+        try (LineNumberReader reader = new LineNumberReader(new BufferedReader(new FileReader(path)));
+             Writer inValid = new FileWriter("/Users/Oleg/Desktop/inValid.txt", false);
+             Writer valid = new FileWriter("/Users/Oleg/Desktop/Valid.txt",true)) {
             String s = reader.readLine();
+            while (s != null) {
+                if (s.length() != 15) {
+                    inValid.write("\nInvalid file's  length, must be 15 symbols but was " + s);
+                    System.out.println("Invalid file's  length");
 
-
-            try {
-                while (s != null) {
-                    if (s.length()!=15){
-                        throw new RuntimeException("Invalid file lenght");
-                    }
-                    if (!s.startsWith("docnum") && !s.startsWith("contract")){
-                        throw new RuntimeException("Invalid file prefix");
-                    }
-                    System.out.println(s);
-                    s = reader.readLine();
                 }
-            }finally {
-                reader.close();
+                else if (!s.startsWith("docnum") && !s.startsWith("contract")) {
+                    inValid.write("\nInvalid file's  name, must be 'docnum' or 'contract' prefix but was " + s);
+                    System.out.println("Invalid file's name ");
+
+                }
+                else {
+                valid.write("\n"+s);
+
+                }
             }
-        }
-        catch (IOException exc) {
+        } catch (IOException exc) {
             System.out.println(exc.getMessage());
         }
     }
