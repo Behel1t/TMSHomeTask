3d22056a089073f5a46a9023c17eb6cd0ea574e1
Oleg <01technique@gmail.com>
2023-08-13 18:46:23 +0300

    Commit

---
+++ lesson11/src/main/java/org/example/tms/MainApp.java
@@ -0,0 +1,20 @@
+package org.example.tms;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        MyCollection<Car>carMyCollection=new MyCollectionImpl<>();
+        carMyCollection.add(new Car("red",2000,400));
+        MyCollection<Plane>planeMyCollection=new MyCollectionImpl<>();
+        planeMyCollection.add(new Plane("white",46700,2334));
+        System.out.println(carMyCollection);
+        System.out.println(planeMyCollection);
+
+
+
+
+
+
+
+    }
+}
