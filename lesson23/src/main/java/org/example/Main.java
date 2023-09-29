cd12ea773a1a9f4ec4061db3bb12e73f23c974a8
Behel1t <139507089+Behel1t@users.noreply.github.com>
2023-09-29 21:43:59 +0300

    Commit (#43)

---
+++ lesson23/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+        UUID uuid=UUID.randomUUID();
+        UUID.fromString("a2542198-4a1f-4976-abe8-26c51bef16a4");
+        System.out.println(uuid);
+    }
+}
