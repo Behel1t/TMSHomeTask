ca801f23865a559a691133c42ad9f9584de4ebaa
Oleg <01technique@gmail.com>
2024-01-01 18:08:29 +0300

    Commit

---
+++ lesson43bl/src/main/java/com/example/lesson43bl/web/CarsController.java
@@ -0,0 +1,49 @@
+package com.example.lesson43bl.web;
+
+import com.example.lesson43bl.dto.CarDto;
+import com.example.lesson43bl.dto.SearchDto;
+import com.example.lesson43bl.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cars")
+public class CarsController {
+    private final CarService service;
+    @PostMapping
+    public CarDto save(@RequestBody CarDto dto){
+        return service.save(dto);
+    }
+    @GetMapping
+    public List<CarDto> find(){
+        return service.findAll();
+    }
+    @GetMapping("/{id}")
+    public CarDto find(@PathVariable(name = "id") Integer id){
+        return service.find(id);
+    }
+    @PutMapping("/{id}")
+    public CarDto update(@PathVariable(name = "id") Integer id,@RequestBody CarDto request){
+        return service.update(id,request);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable(name = "id") Integer id) {
+        service.delete(id);
+    }
+    @PostMapping("/buy/{id}")
+    public CarDto buy(@PathVariable(name = "id")Integer id){
+        return service.buy(id);
+    }
+    @PostMapping("/find")
+    public List<CarDto> find(@RequestBody SearchDto searchDto){
+        return service.find(searchDto);
+    }
+    @GetMapping("/resolve/{name}")
+    public CarDto findByName(@PathVariable(name = "name") String name){
+        return service.findByName(name);
+    }
+
+}
